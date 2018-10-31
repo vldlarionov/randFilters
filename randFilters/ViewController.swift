18599ca851652ed1de9575548c9bb76ed455d9af
vbox <vbox@vboxs-imac.local>
2018-10-31 19:55:14 -0400

    Algoritm for applying filters added

--- randFilters/ViewController.swift
+++ randFilters/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let contex = CIContext(options: nil)
+    
+    
+    @IBAction func applyFilter(_ sender: Any) {
+        
+        let inputImage = CIImage(image: photoImageView.image!)
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        let filteredImage = inputImage?.applyingFilter("CIHueAdjust", parameters: randomColor)
+        let renderedImage = contex.createCGImage(filteredImage!, from: filteredImage!.extent)
+        photoImageView.image = UIImage(cgImage: renderedImage!)
+    }
+    
+    
+ 
+    @IBAction func showNotFilteredImage(_ sender: Any) {
+        
+        photoImageView.image = #imageLiteral(resourceName: "cat.jpg")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
